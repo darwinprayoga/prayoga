219ef49036471ec1189986791bf626495c5855f6
darwinprayoga <darwin.prayoga13@gmail.com>
2026-05-19 11:26:44 +0800

    fix: replace twind with standard tailwind to fix hydration

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>prayoga</title>
+        <link rel="stylesheet" href="/styles.min.css" />
       </head>
       <body>
         <Component />
